db9a0b358663fb174ac72c25a7da943c7805c7a7
vhalchuk.appwell <vhalchuk.appwell@ancestry.com>
2025-01-26 12:01:11 +0400

    Add textarea

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,12 @@ function App() {
             <aside className="h-full w-[260px] bg-gray-900"></aside>
             <main className="flex h-full grow flex-col bg-gray-800">
                 <div className="flex-1 overflow-hidden">Messages section</div>
-                <div className="">Input section</div>
+                <div className="p-4">
+                    <textarea
+                        placeholder="Message AI"
+                        className="block h-10 w-full resize-none border-0 bg-transparent p-2"
+                    />
+                </div>
             </main>
         </div>
     );
