430962b47ba59b3d2f4d183ba734879b86368eb0
vhalchuk.appwell <vhalchuk.appwell@ancestry.com>
2025-01-26 12:24:05 +0400

    Add react-compiler

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-  },
-})
+    plugins: [
+        react({
+            babel: {
+                plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+            },
+        }),
+        tailwindcss(),
+    ],
+    server: {
+        host: "0.0.0.0",
+        port: 5173,
+    },
+});
