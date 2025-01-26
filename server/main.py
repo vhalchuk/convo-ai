7649ed6d7b73bb1ae43567764e0012ae7ffb555a
Artem Beshevli <artem.beshevli@gmail.com>
2025-01-26 11:36:34 +0100

    Add basic server

--- server/main.py
+++ server/main.py
@@ -1,5 +1,5 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-
+import json
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -10,7 +10,31 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
         # Send the plain text response body
         self.wfile.write("<h1>Hello World!</h1>".encode('utf-8'))
+    def do_POST(self):
+        # Read the length of the incoming data
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)  # Read the received POST data
 
+        # Decode the incoming data (assuming it's JSON)
+        try:
+            received_data = json.loads(post_data)
+            question = received_data.get('question', '')  # Extract "question" value
+        except json.JSONDecodeError:
+            question = ''
+
+        # Construct a response
+        response = {
+            "message": "Answer received!",
+            "duplicated_question": question if question else ""
+        }
+
+        # Send a 200 OK response with a JSON content type
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+
+        # Send the JSON response
+        self.wfile.write(json.dumps(response).encode('utf-8'))
 
 # Create the server
 server_address = ('0.0.0.0', 8000)  # Address and port
