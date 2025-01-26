c34753513636136dbff47d5b47c926a8c1ab4b38
vhalchuk.appwell <vhalchuk.appwell@ancestry.com>
2025-01-26 11:55:52 +0400

    Add page layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,13 @@
 function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <div className="flex h-full w-full overflow-hidden">
+            <aside className="h-full w-[260px] bg-gray-900"></aside>
+            <main className="flex h-full grow flex-col bg-gray-800">
+                <div className="flex-1 overflow-hidden">Messages section</div>
+                <div className="">Input section</div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
