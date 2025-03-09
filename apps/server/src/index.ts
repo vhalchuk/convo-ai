ebf9901312b42c7182e7f97d6bf9b03ebe9ba790
vhalchuk.appwell <vhalchuk.appwell@ancestry.com>
2025-03-09 18:28:08 +0400

    chore: try connecting to OpenAI API

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import OpenAI from "openai";
 import router from "./router";
 import { env } from "./env";
 
@@ -10,3 +11,15 @@ app.use(router);
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
+
+(async () => {
+    const client = new OpenAI({
+        apiKey: env.OPENAI_API_KEY
+    })
+    const chatCompletion = await client.chat.completions.create({
+        messages: [{ role: 'user', content: 'Say this is a test' }],
+        model: 'gpt-4o',
+    });
+
+    console.log(chatCompletion.choices[0]?.message.content);
+})();
