c155f85f4dae329e6ea24fdecebd0b70ff497e93
vhalchuk.appwell <vhalchuk.appwell@ancestry.com>
2025-03-10 18:03:54 +0400

    chore: setup workspaces

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,25 +1,36 @@
-import { defineConfig } from "vite";
-import path from "path";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig, loadEnv, splitVendorChunkPlugin } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [
-        react({
-            babel: {
-                plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+export default defineConfig(({ mode }) => {
+    // Load environment variables based on the current mode.
+    const env = loadEnv(mode, process.cwd(), "");
+    const isAnalyze = env.ANALYZE === "true";
+
+    return {
+        plugins: [
+            react({
+                babel: {
+                    plugins: [
+                        ["babel-plugin-react-compiler", { target: "19" }],
+                    ],
+                },
+            }),
+            tailwindcss(),
+            splitVendorChunkPlugin(),
+            ...(isAnalyze ? [visualizer({ open: true })] : []),
+        ],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
             },
-        }),
-        tailwindcss(),
-    ],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
         },
-    },
-    server: {
-        host: "0.0.0.0",
-        port: 5173,
-    },
+        server: {
+            host: "0.0.0.0",
+            port: 5173,
+        },
+    };
 });
