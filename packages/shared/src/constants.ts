4c7f49c163eacac5368e93cb4f98f5c80728b4d7
Volodymyr Halchuk <volodymyr.halchuk.dev@gmail.com>
2025-04-27 17:32:11 +0400

    refactor: remove unused SSE_EVENTS constants

--- packages/shared/src/constants.ts
+++
@@ -1,9 +0,0 @@
-export const SSE_EVENTS = {
-    OPEN: "open",
-    ERROR: "error",
-    MESSAGE: "message",
-    DELTA: "delta",
-    CONVERSATION_NAME: "conversation_name",
-    SERVER_ERROR: "server_error",
-    MODEL_INFO: "model_info",
-} as const;
